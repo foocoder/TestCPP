748628e807a6833d45cc1a1a4709f9083e901962
foocoder <slow295185031@gmail.com>
2016-03-24 18:37:32 +0800

    add test Iterator erase

--- testIteratorErase/testIteratorErase.cpp
+++ testIteratorErase/testIteratorErase.cpp
@@ -10,40 +10,48 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
-template<typename elemType>
-void display(const vector<elemType> & vec){
-    typename vector<elemType>::const_iterator iter = vec.begin();
-    typename vector<elemType>::const_iterator end_it = vec.end();
-    for(iter; iter!=end_it; iter++){
-        cout<<*iter<<" ";
+template<typename iteratorType>
+void display(iteratorType first, iteratorType last){
+    for(;first!=last; first++){
+        cout<<*first<<" ";
     }
     cout<<endl;
 }
 
 int main(int argc, char *argv[])
 {
+    int ia[] = {0,1,1,2,3,5,8,13,21,55,89};
     vector<int> iVec;
+    list<int> iList;
 
     vector<int>::iterator it;
+    list<int>::iterator ilter;
 
     for(int i=0;i<10;i++)
     {
         iVec.push_back(i);
-        iVec.push_back(i);
+        iList.push_back(i);
     }
 
-    display(iVec);
-
+    display(iVec.begin(), iVec.end());
     for(it=iVec.begin();it!=iVec.end();++it) {
-        if(*it ==4 || *it == 7)
+        if(!(*it & 1))
         {
-            iVec.erase(it++);
+            iVec.erase(it--);
         }
     }
+    display(iVec.begin(), iVec.end());
 
-    display(iVec);
+    for(ilter=iList.begin();ilter!=iList.end();++ilter) {
+        if(!(*ilter & 1))
+        {
+            iList.erase(ilter--);
+        }
+    }
+    display(iList.begin(),iList.end());
     return 0;
 }
