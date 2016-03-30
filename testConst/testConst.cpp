fa4d9b16d3c784b04dd37b04ec4536e05b155820
foocoder <slow295185031@gmail.com>
2016-03-30 19:53:10 +0800

    add test const

---
+++ testConst/testConst.cpp
@@ -0,0 +1,19 @@
+// ---- Program Info Start----
+//FileName:     testConst.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-03-30 19:43:00
+// ---- Program Info End  ----
+
+enum test{ aa };
+int main(int argc, char *argv[])
+{
+    const int a = 0;
+    int b = 0;
+    test c = aa;
+    return 0;
+}
+
