350aa9749616dd062df3e7ed74335e5426c784d2
foocoder <slow295185031@gmail.com>
2016-03-30 18:18:17 +0800

    Test All the Type sizeof

--- testSizeof/testSizeof.cpp
+++ testSizeof/testSizeof.cpp
@@ -174,5 +174,16 @@ int main(int argc, char *argv[])
     char arrDyChar[n];
     cout<<"sizeof(arrDyChar[n]): "<<sizeof(arrDyChar)<<endl;
 
+    // Test New Type
+    cout<<"sizeof(wchar_t): "<<sizeof(wchar_t)<<endl;
+    cout<<"sizeof(char16_t): "<<sizeof(char16_t)<<endl;
+    cout<<"sizeof(char32_t): "<<sizeof(char32_t)<<endl;
+    cout<<"sizeof(short): "<<sizeof(short)<<endl;
+    cout<<"sizeof(long): "<<sizeof(long)<<endl;
+    cout<<"sizeof(long long): "<<sizeof(long long)<<endl;
+    cout<<"sizeof(float): "<<sizeof(float)<<endl;
+    cout<<"sizeof(double): "<<sizeof(double)<<endl;
+    cout<<"sizeof(long double): "<<sizeof(long double)<<endl;
+
     return 0;
 }
