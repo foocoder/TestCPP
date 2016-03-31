717e15ec22e3fe9eb7087f0046e1f32b7292048c
foocoder <slow295185031@gmail.com>
2016-03-31 16:57:51 +0800

    add test cosnt in class

--- testConstInClass/testConstInClass.cpp
+++ testConstInClass/testConstInClass.cpp
@@ -27,11 +27,36 @@ class B{
     public:
     void FuncConst() const{
         _a.FuncConst();
+        //_a.FuncNonConst();//Error! Can't Compiled!
     }
 };
+
+class CTextBlock{
+    public:
+        //CTextBlock():pText(new char[6]{'H','e','l','l','o','\0'}){}
+        const char * getText() const
+        { return pText; }
+        const char & operator[](size_t position) const
+        { return pText[position]; }
+        ~CTextBlock(){
+            delete[] pText;
+        }
+    private:
+        const char * pText = "Hello";
+};
+
+ostream & operator<<(ostream &os, const CTextBlock & rhs){
+    os<<rhs.getText();
+    return os;
+}
 
 int main(int argc, char *argv[])
 {
 
+    const CTextBlock cctb;
+    //char * pc = &cctb[0];
+    //*pc = 'a';
+    cctb[0] = 'a';
+    cout<<cctb<<endl;
     return 0;
 }
