c7e71737988d87487d50cbd19bc62fcde5568161
foocoder <slow295185031@gmail.com>
2016-05-26 20:51:02 +0800

    add test memory leak

--- testMemoryLeak/testMemoryLeak.cpp
+++ testMemoryLeak/testMemoryLeak.cpp
@@ -9,10 +9,13 @@
 // ---- Program Info End  ----
 
 #include <iostream>
+#include <malloc.h>
 
 using namespace std;
 
 class simplePOD{
+    public:
+        simplePOD():_val(0xCDCDCDCD){}
     private:
         int _val;
 };
@@ -24,7 +27,7 @@ class arrPOD{
 
 class complexStruct{
     public:
-        complexStruct():_ptr_val(new int(0)){ }
+        complexStruct():_ptr_val(new int(0xCDCDCDCD)){ }
         ~complexStruct(){
             delete _ptr_val;
         }
@@ -34,16 +37,59 @@ class complexStruct{
 
 int main(int argc, char *argv[])
 {
-    int * ptrInt = new int[16];
-    delete ptrInt; //No Memory Leak
+    //int arrInt[100] = {0};
+    //delete arrInt; // Runtime Error: Segment Fault. Free a pointer on stack;
 
-    //simplePOD *ptrSimplePOD = new simplePOD[16];
+    //cout<<"\nBEFORE NEW"<<endl;
+    //malloc_stats();
+    //int * ptrIntMalloc = (int *) malloc(10*sizeof(int)); //情况与下边new int[10]一致
+    //for(int i=0; i<10; ++i)
+        //ptrIntMalloc[i] = 0xCDCDCDCD;
+    //cout<<"\nAFTER NEW"<<endl;
+    //malloc_stats();
+    //delete ptrIntMalloc; //No Memory Leak
+    //cout<<"\nAFTER DELETE"<<endl;
+    //malloc_stats();
+
+    //cout<<"\nBEFORE NEW"<<endl;
+    //malloc_stats();
+    //int * ptrInt = new int[10](); // malloc 最小内存块32bytes, 每次增加16bytes。需要8bytes的额外空间。
+    //for(int i=0; i<10; ++i)
+        //ptrInt[i] = 0xCDCDCDCD;
+    //cout<<"\nAFTER NEW"<<endl;
+    //malloc_stats();
+    //delete ptrInt; //No Memory Leak
+    //cout<<"\nAFTER DELETE"<<endl;
+    //malloc_stats();
+
+    //cout<<"\nBEFORE NEW"<<endl;
+    //malloc_stats();
+    //simplePOD *ptrSimplePOD = new simplePOD[10]();
+    //cout<<"\nAFTER NEW"<<endl;
+    //malloc_stats();
     //delete ptrSimplePOD; //No Memory Leak
+    //cout<<"\nAFTER DELETE"<<endl;
+    //malloc_stats();
 
-    //arrPOD * ptrArrPOD = new arrPOD[16];
+    //cout<<"\nBEFORE NEW"<<endl;
+    //malloc_stats();
+    //arrPOD * ptrArrPOD = new arrPOD[10];
+    //cout<<"\nAFTER NEW"<<endl;
+    //malloc_stats();
     //delete ptrArrPOD; //No Memory Leak
+    //cout<<"\nAFTER DELETE"<<endl;
+    //malloc_stats();
+    //cout<<sizeof(arrPOD) <<endl;
 
-    //complexStruct * ptrComplexStruct = new complexStruct[16];
-    //delete ptrComplexStruct;
+    cout<<"\nBEFORE NEW"<<endl;
+    malloc_stats();
+    complexStruct * ptrComplexStruct = new complexStruct[10];
+    cout<<"\nAFTER NEW"<<endl;
+    malloc_stats();
+    delete ptrComplexStruct; //Memory Leak;
+    cout<<"\nAFTER DELETE"<<endl;
+    malloc_stats();
+    cout<<sizeof(complexStruct) <<endl;
+
     return 0;
 }
