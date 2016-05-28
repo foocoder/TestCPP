37bcd8914547b05d6bda41fb7b35039eb796ee34
foocoder <slow295185031@gmail.com>
2016-05-28 20:13:25 +0800

    add test segment in a.out

---
+++ testSegmentInAdotOut/testSegmentInAdotOut.cpp
@@ -0,0 +1,19 @@
+// ---- Program Info Start----
+//FileName:     testSegmentInAdotOut.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-05-28 19:41:30
+// ---- Program Info End  ----
+
+#include <stdio.h>
+
+int gnArr[1000] = {1,1,1,1};
+int main(int argc, char *argv[])
+{
+    int lnArr[1000] = {1,1};
+    printf("Hello World\n");
+    return 0;
+}
