9c3ddf7ddc3a329ecae36394c52524881cdf77ef
foocoder <slow295185031@gmail.com>
2016-07-14 18:23:19 +0800

    add test how to start up

--- testHowtoStartUp/testHowtoStartUp.cpp
+++ testHowtoStartUp/testHowtoStartUp.cpp
@@ -8,6 +8,21 @@
 //CreatedAt:    2016-07-12 15:56:34
 // ---- Program Info End  ----
 
-int main(){
+#include <stdio.h>
 
+void __attribute__ (( constructor )) a_constructor(int i){
+    printf("%s\n%d\n", __FUNCTION__, i);
 }
+void __attribute__ (( constructor )) a_constructor1(int i){
+    printf("%s\n%d\n", __FUNCTION__, i);
+}
+
+int main(int argc, char * argv[]){
+    printf("%s\n", __FUNCTION__);
+    return 0;
+}
+
+void __attribute__ (( destructor )) a_destructor(){
+    printf("%s\n", __FUNCTION__);
+}
+
