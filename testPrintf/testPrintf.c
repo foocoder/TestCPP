2fde64b420e5194448077f7b1d1eb078a03bf01e
foocoder <slow295185031@gmail.com>
2016-07-15 16:43:06 +0800

    add test printf

---
+++ testPrintf/testPrintf.c
@@ -0,0 +1,21 @@
+// ---- Program Info Start----
+//FileName:     testPrintf.c
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-07-15 16:39:20
+// ---- Program Info End  ----
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a=1000;
+    double b=1000.0;
+    char c=100;
+    printf(" %d %f %c \n", a,b,c);
+    printf(" %f %c %f \n", a,b,c);
+    return 0;
+}
