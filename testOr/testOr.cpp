7de4854ca5f88fd2c1854579cc62ada86f80f7ad
foocoder <slow295185031@gmail.com>
2017-02-24 16:31:56 +0800

    test or key words

---
+++ testOr/testOr.cpp
@@ -0,0 +1,21 @@
+// ---- Program Info Start----
+//FileName:     testOr.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2017-02-24 16:30:20
+// ---- Program Info End  ----
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if( 1 and 2 ){
+        cout<<"OK!"<<endl;
+    }
+    return 0;
+}
